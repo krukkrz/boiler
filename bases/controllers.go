2d546976821363e50accda937531ed9212bf5a52
Krzysztof Kruk <krzysztof.kruk@digitalnewagency.com>
2021-06-26 15:34:16 +0200

    getAll ready

--- bases/controllers.go
+++ bases/controllers.go
@@ -17,8 +17,11 @@ func GetControllerBody() string {
 			requestMapping().Import,
 			allArgsConstructor().Import,
 			postMapping("").Import,
+			getMapping("").Import,
 			requestBody().Import,
 			responseEntity().Import,
+			list().Import,
+			collectors().Import,
 		},
 		[]string{
 			restController().Content,
@@ -35,6 +38,13 @@ func GetControllerBody() string {
 			postMapping("").Content,
 			newLine(),
 			createMethod(),
+			newLine(),
+
+			newLine(),
+			tab(),
+			getMapping("").Content,
+			newLine(),
+			getAllMethod(),
 
 			endClass(),
 		},
@@ -42,9 +52,20 @@ func GetControllerBody() string {
 	return concatenateBody(content)
 }
 
+func getAllMethod() string {
+	signature := responseEntity().Content + "<" + list().Content + "<" + generatedDto().Content + ">> getAll()"
+	returnLine := "return " + responseEntity().Content + ".ok("
+	serviceStream1 := newLine() + tab() + tab() + tab() + "service.getAll()"
+	serviceStream2 := newLine() + tab() + tab() + tab() + tab() + ".stream()"
+	serviceStream3 := newLine() + tab() + tab() + tab() + tab() + ".map(" + Domain + "Mapper::" + staticToDto().Content + ")"
+	serviceStream4 := newLine() + tab() + tab() + tab() + tab() + ".collect(" + collectors().Content + ".toList()))"
+	serviceStream := returnLine + serviceStream1 + serviceStream2 + serviceStream3 + serviceStream4
+	return method(signature, serviceStream)
+}
+
 func createMethod() string {
 	domain := strings.ToLower(Domain)
-	signature := generatedEntity().Content + " create(" + requestBody().Content + " " + generatedDto().Content + " " + domain + "Dto)"
+	signature := responseEntity().Content + "<" + generatedEntity().Content + "> create(" + requestBody().Content + " " + generatedDto().Content + " " + domain + "Dto)"
 	toModel := generatedEntity().Content + " " + domain + " = " + staticToModel().Content + "(" + domain + "Dto)"
 	savedIteration := generatedEntity().Content + " saved" + Domain + " = service.create(" + domain + ")"
 	returnMethod := "return " + responseEntity().Content + ".ok(" + staticToDto().Content + "(saved" + Domain + "))"
@@ -55,5 +76,12 @@ func responseEntity() Class {
 	return Class{
 		"import org.springframework.http.ResponseEntity;\n",
 		"ResponseEntity",
+	}
+}
+
+func collectors() Class {
+	return Class{
+		"import java.util.stream.Collectors;\n",
+		"Collectors",
 	}
 }
